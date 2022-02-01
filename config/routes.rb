ef78a806b270aec091a9228eb57ab65f5090440b
Manuj Patel <manuj.p@simformsolutions.com>
2022-02-01 10:37:55 +0530

    created student and faculty model and genrated controller for it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'faculties/index'
+  get 'faculties/new'
+  get 'faculties/create'
+  get 'faculties/show'
+  get 'faculties/edit'
+  get 'faculties/update'
+  get 'faculties/destroy'
+  get 'students/index'
+  get 'students/new'
+  get 'students/create'
+  get 'students/show'
+  get 'students/edit'
+  get 'students/update'
+  get 'students/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
