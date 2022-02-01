21cc267b9a741a6f3cf9f87251886d72fa1948d7
Manuj Patel <manuj.p@simformsolutions.com>
2022-02-01 22:58:14 +0530

    Added terms of of usage to student

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,8 +1,15 @@
 class Faculty < ApplicationRecord
     validates :first_name, :last_name, :email, presence: true
-    validates :first_name, :last_name, :email, uniqueness: true
+    validates :phone, :email, uniqueness: true
     validates :phone, numericality: true
     validates :phone, length: {is: 10}
+    validates :designation, exclusion: { in: %w(HOD Sr.Prof)}
+    validates :designation, inclusion: { in: %w(Ass.Prof Prof), message: "designation can't be %{value}"}
+    validate :date_of_birth_cannot_be_in_the_future
 
-
+    def date_of_birth_cannot_be_in_the_future
+        if date_of_birth > Date.today
+        errors.add(:date_of_birth, "can't be in the future")
+        end
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,15 @@
 class Student < ApplicationRecord
     validates :first_name, :last_name, presence: true
-    validates :first_name, :last_name, uniqueness: true
+    validates :department, inclusion: { in: %w(IT CE), message: "department can't be %{value}"}
+    validate :date_of_birth_cannot_be_in_the_future
+
+    def date_of_birth_cannot_be_in_the_future
+        if date_of_birth > Date.today
+        errors.add(:date_of_birth, "can't be in the future")
+        end
+    end
+
+    validates :terms_of_usage, acceptance: { message: 'must be accepted' } 
+
 
 end
