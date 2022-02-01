6978c0b76f91c809336190fa60841858b31d4fe4
Manuj Patel <manuj.p@simformsolutions.com>
2022-02-01 10:50:38 +0530

    Add validation to name and phone number

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,2 +1,8 @@
 class Faculty < ApplicationRecord
+    validates :first_name, :last_name, :email, presence: true
+    validates :first_name, :last_name, :email, uniqueness: true
+    validates :phone, numericality: true
+    validates :phone, length: {is: 10}
+
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,5 @@
 class Student < ApplicationRecord
+    validates :first_name, :last_name, presence: true
+    validates :first_name, :last_name, uniqueness: true
+
 end
